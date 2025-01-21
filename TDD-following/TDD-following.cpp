8096b54e399e5f5121b236b0148e6476bfc55000
Jun <cowzon90@gmail.com>
2025-01-22 08:22:30 +0900

    :tada:: Init Test-Driven-Development following project

---
+++ TDD-following/TDD-following.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TDDfollowing
+{
+	TEST_CLASS(TDDfollowing)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
